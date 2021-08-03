86ce0e72976c1b8f481887802b6e1676bdd5dd07
Eduardo Peschke <eduardopeschke@gmail.com>
2021-08-03 12:49:26 -0400

    Add Types

--- pokemonAPI/main.js
+++ pokemonAPI/main.js
@@ -20,26 +20,41 @@ function fecthPokemons(number) {
 
 // Create Pokemon
 function createPokemon(pokemon) {
+    // Card
     const card = document.createElement('div')
     card.classList.add('pokemon-block')
 
+    // Sprite Contanier
     const spriteContainer = document.createElement('div')
     spriteContainer.classList.add('img-container')
 
+    // Sprite
     const sprite = document.createElement('img')
     sprite.src = pokemon.sprites.front_default
 
     spriteContainer.appendChild(sprite)
 
+    // ID Pokemon
     const number = document.createElement('p')
     number.textContent = `#${pokemon.id.toString().padStart(3,0)}`
 
+    // Name Pokemon
     const name = document.createElement('p')
     name.textContent = pokemon.name 
 
+    // Types Pokemon
+    const typeOne = document.createElement('p')
+    typeOne.textContent = pokemon.types[0].type.name
+
+    const typeTwo = document.createElement('p')
+    typeTwo.textContent = pokemon.types[1].type.name
+
+    // Add Elements To Card
     card.appendChild(spriteContainer)
     card.appendChild(number)
     card.appendChild(name)
+    card.appendChild(typeOne)
+    card.appendChild(typeTwo)
 
     pokemonContainer.appendChild(card)
 }
